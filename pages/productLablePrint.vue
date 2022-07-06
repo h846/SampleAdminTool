6c3a2755a90272d0dbf7ec8f31cb1d2117402de3
Hideki Yashiro <Hideki.Yashiro@landsend.co.jp>
2022-07-06 16:51:30 +0900

    modified product print

--- pages/productLablePrint.vue
+++ pages/productLablePrint.vue
@@ -1,39 +1,41 @@
 <template>
-<div class="sheets">
-  <v-row>
-    <v-col cols="6" v-for='(item, idx) in printList' :key='idx'>
-  <div class="sheet" >
-  <div>
-    <span>商品番号: </span><span>{{item.STY_NUM}}</span>
+  <div class="sheets">
+    <div class="sheet">
+      <v-row no-gutters>
+        <v-col cols="6" v-for="(item, idx) in printList" :key="idx">
+          <div class="cell">
+            <div>
+              <span>商品番号: </span><span>{{ item.STY_NUM }}</span>
+            </div>
+            <div>
+              <span>商品名: </span><span>{{ item.STY_NAME_JP }}</span>
+            </div>
+            <div>
+              <span>カラー: </span><span>{{ item.CLR_DSC_JP }}</span>
+            </div>
+            <div>
+              <span>サイズ: </span><span>{{ item.SIZ }}</span>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+    </div>
   </div>
-  <div>
-    <span>商品名: </span><span>{{item.STY_NAME_JP}}</span>
-  </div>
-  <div>
-    <span>カラー: </span><span>{{item.CLR_DSC_JP}}</span>
-  </div>
-  <div>
-    <span>サイズ: </span><span>{{item.SIZ}}</span>
-  </div>
-  </div>
-  </v-col>
-  </v-row>
-</div>
 </template>
 
 <script>
 export default {
-  layout:'empty',
-  computed:{
-    printList: function(){
-      return this.$store.state.sampleData.filter(val=>{
-        return val.STY_PRINT_FLG === 1;
+  layout: 'empty',
+  computed: {
+    printList: function () {
+      return this.$store.state.sampleData.filter((val) => {
+        return val.STY_PRINT_FLG === 1
       })
-    }
+    },
   },
-  mounted(){
+  mounted() {
     console.log(this.printList)
-  }
+  },
 }
 </script>
 <style lang="scss" scoped>
@@ -43,18 +45,28 @@ export default {
 }
 
 /* hide in print */
-@media print {
-  .sheets > :not(.sheet) {
-    display: none;
-  }
-  .sheet {
-    display: block;
-    padding-top: 6mm;
-    padding-left: 6mm;
-    font-family: 'Sawarabi Mincho', serif;
-    font-size: 20px;
+// @media print {
+.sheets > :not(.sheet) {
+  display: none;
+}
+.sheet {
+  width: 210mm;
+  height: 297mm;
+  display: block;
+  padding: 21.2mm 18.6mm;
+  font-family: 'Sawarabi Mincho', serif;
+  font-size: 14px;
+  .cell {
+    padding: 3mm;
+    width: 80mm;
+    height: 50.8mm;
+    border: 1px solid #ccc;
+    div {
+      margin-bottom: 3mm;
+    }
   }
 }
+//}
 
 /* for preview */
 /*
@@ -65,10 +77,3 @@ export default {
 }
 */
 </style>
-<style lang="scss">
-/* for preview */
-@media screen {
-  BODY {
-    background: #eee;
-  }
-}

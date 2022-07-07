5814dfbdea701b78f5d22e70194de1538253c43c
Yashiro <Hideki.Yashiro@landsend.co.jp>
2022-07-07 17:10:24 +0900

    modified product label

--- pages/productLablePrint.vue
+++ pages/productLablePrint.vue
@@ -8,7 +8,7 @@
               <span>商品番号: </span><span>{{ item.STY_NUM }}</span>
             </div>
             <div>
-              <span>商品名: </span><span>{{ item.STY_NAME_JP }}</span>
+              <span>{{ item.STY_NAME_JP }} </span>
             </div>
             <div>
               <span>カラー: </span><span>{{ item.CLR_DSC_JP }}</span>
@@ -19,6 +19,9 @@
           </div>
         </v-col>
       </v-row>
+    </div>
+    <div style="text-align: center; padding-top: 50px">
+      <v-btn to="/" nuxt x-large>印刷済</v-btn>
     </div>
   </div>
 </template>
@@ -35,45 +38,46 @@ export default {
   },
   mounted() {
     console.log(this.printList)
+    window.print()
   },
 }
 </script>
 <style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css2?family=Sawarabi+Mincho&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Kiwi+Maru&display=swap');
+body {
+  width: 100%;
+}
 .sheet {
   page-break-after: always;
 }
 
 /* hide in print */
-// @media print {
-.sheets > :not(.sheet) {
-  display: none;
-}
-.sheet {
-  width: 210mm;
-  height: 297mm;
-  display: block;
-  padding: 21.2mm 18.6mm;
-  font-family: 'Sawarabi Mincho', serif;
-  font-size: 14px;
-  .cell {
-    padding: 3mm;
-    width: 80mm;
-    height: 50.8mm;
-    border: 1px solid #ccc;
-    div {
-      margin-bottom: 3mm;
+@media print {
+  .sheets > :not(.sheet) {
+    display: none;
+  }
+  .sheet {
+    width: 210mm;
+    height: 297mm;
+    display: block;
+    padding: 21.2mm 18.6mm;
+    font-family: 'Kiwi Maru', serif;
+    font-size: 15px;
+    .cell {
+      padding: 5mm 3mm;
+      width: 86.4mm;
+      height: 50.8mm;
+      // border: 1px solid #ccc;
+      div {
+        margin-bottom: 2mm;
+      }
     }
   }
 }
-//}
-
 /* for preview */
-/*
 @media screen {
   .sheet {
     display: none;
   }
 }
-*/
 </style>
